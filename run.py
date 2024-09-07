6780a1e9412bc64ca5fe6b8734b8001bf83de8fc
Megan Saenz <saenzmegan@ymail.com>
2024-09-07 14:25:03 +0000

    Add function to show word guesses

--- run.py
+++ run.py
@@ -7,3 +7,10 @@ def select_random_word(word_list)
     Selects a random word from the provided word list.
     """
     return random.choice(word_list)
+
+def display_word_progress(word, guessed_letters):
+    """
+    Displays the currenty progress of the word being guessed.
+    Shows correct guesses and underscores for the remaining letters.
+    """
+    return " ".join([letter if letter in guessed_letters else "_" for letter in word])

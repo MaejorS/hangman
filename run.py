1fcf15e67412d40bf0b96f5120d8d6ab1e96172f
Megan Saenz <saenzmegan@ymail.com>
2024-09-07 14:21:39 +0000

    Create function that picks random word

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 import random
 
 WORD_LIST = ["magic", "manifestation", "cosmic", "destiny", "fate", "divine", "spirit", "abundance"]
+
+def select_random_word(word_list)
+    """
+    Selects a random word from the provided word list.
+    """
+    return random.choice(word_list)
